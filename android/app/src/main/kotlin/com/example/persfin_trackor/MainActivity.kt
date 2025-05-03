fbfdb822b205abd056698fc4ce2d6cc72b7f50a1
Sudha-sivaraj <sudhasept86@gmail.com>
2025-05-03 15:48:50 +0530

    using cards and listview

---
+++ android/app/src/main/kotlin/com/example/persfin_trackor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.persfin_trackor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text('FinanceTracking'),
+
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.insert_chart_outlined_sharp),
+                  text: 'Summary',
+                ),
+                Tab(icon: Icon(Icons.ac_unit_rounded), text: 'Transaction'),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Card(
+                color: Colors.greenAccent,
+                elevation: 10,
+
+                child: Column(
+                  children: [
+                    Card(
+                      color: Colors.lime,
+                      child: Text(
+                        'Income',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text('Expenses'),
+                    Text('Total Income'),
+                  ],
+                ),
+              ),
+              Icon(Icons.ac_unit_rounded),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
